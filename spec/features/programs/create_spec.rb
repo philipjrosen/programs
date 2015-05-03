364203a3afb366e6397a076b67324cea52541bb1
Philip Rosen <philipjrosen@gmail.com>
2015-05-03 14:02:39 -0700

    refactor creating programs in spec

--- spec/features/programs/create_spec.rb
+++ spec/features/programs/create_spec.rb
@@ -1,29 +1,30 @@
 require 'rails_helper'
 
 describe "Creating Programs" do
-  it "redirects to the programs index page on success" do
+  def create_program(options={})
+    options[:title] ||= "Mad Men"
+    options[:subtitle] ||= "The 1960s"
+    options[:code] ||= "mms712"
+
     visit "/programs"
     click_link "New Program"
     expect(page).to have_content("New Program")
 
-    fill_in "Title", with: "Mad Men"
-    fill_in "Subtitle", with: "The 1960s"
-    fill_in "Code", with: "madmen"
+    fill_in "Title", with: options[:title]
+    fill_in "Subtitle", with: options[:subtitle]
+    fill_in "Code", with: options[:code]
     click_button "Create Program"
+  end
+
+  it "redirects to the programs index page on success" do
+    create_program
     expect(page).to have_content("Mad Men")
   end
 
   it "displays an error when the program has not title" do
     expect(Program.count).to eql(0)
 
-    visit "/programs"
-    click_link "New Program"
-    expect(page).to have_content("New Program")
-
-    fill_in "Title", with: ""
-    fill_in "Subtitle", with: "The 1960s"
-    fill_in "Code", with: "madmen"
-    click_button "Create Program"
+    create_program title: ""
 
     expect(page).to have_content("error")
     expect(Program.count).to eql(0)
@@ -35,15 +36,8 @@ describe "Creating Programs" do
   it "displays an error when the code is not an alphanumeric string with exactly 6 characters" do
     expect(Program.count).to eql(0)
 
-    visit "/programs"
-    click_link "New Program"
-    expect(page).to have_content("New Program")
-
-    fill_in "Title", with: "Mad Men"
-    fill_in "Subtitle", with: "The 1960s"
-    fill_in "Code", with: "madmen7"
-    click_button "Create Program"
-
+    create_program code: "mms0712"
+    
     expect(page).to have_content("error")
     expect(Program.count).to eql(0)
 
